b40b2ce1120484a978ce329d92c080d12feec980
VanessaPetring <50596201+VanessaPetring@users.noreply.github.com>
2019-05-15 11:13:29 +0200

    Add files via upload

---
+++ TestenAnime/app.R
@@ -0,0 +1,54 @@
+library(shiny)
+tidy_anime <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-23/tidy_anime.csv")
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Animes"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("y",
+                        "Y-axis",
+                        choices = c("Score" = "score", "Rank"="rank", "Popularity" = "popularity"),
+                        selected = "score"),
+            selectInput("x",
+                        "x-axis",
+                        choices = c("Score" = "score", "Rank"="rank", "Popularity" = "popularity"),
+                        selected = "popularity"),
+            checkboxGroupInput(inputId = "selected_type",
+                               label = "Select genres:",
+                               choices = c("Action", "Adventure", "comedy", "Drama"),
+                               selected = "Action")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput(outputId = "scatterplot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    animes_subset <- reactive({
+      req(input$selected_type)  
+        filter(tidy_anime, genre %in% input$selected_type)
+    })
+    
+    pretty_plot_title <- toTitleCase(input$plot_title)
+    
+# Scatterplot erstellen
+    output$scatterplot <- renderPlot({
+        ggplot(data = animes_subset, 
+               aes_string(x = input$x, y = input$y)) +
+            geom_point() +
+            labs(title = pretty_plot_title)
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
